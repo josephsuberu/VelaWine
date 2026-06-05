52ac56a3a1f81f7fa4cce18ddf8b95d791a0dc00
FrontendSuberu <joeysuberu@gmail.com>
2026-06-05 13:00:06 +0100

    created component for easy route switching in dev mode

---
+++ src/tests/DropdownMenu/DropdownMenu.jsx
@@ -0,0 +1,39 @@
+import { useNavigate } from "react-router-dom";
+import "./dropdown.css"
+
+const DropdownMenu = () => {
+  const componentMeta = [
+    {
+      name: "Home",
+      path: "/",
+    },
+    {
+      name: "Slider component",
+      path: "/exp/slider",
+    },
+    {
+      name: "duplicate text effect",
+      path: "exp/text-effect",
+    },
+    {
+      name: "inline detail reveal",
+      path: "/exp/inline",
+    },
+  ];
+
+  const navigate = useNavigate();
+
+  return (
+    <select className="dp-box" onChange={(e) => navigate(e.target.value)}>
+      {componentMeta.map((cmt, idx) => {
+        return (
+          <option key={`opt-${idx}`} value={cmt.path}>
+            {cmt.name}
+          </option>
+        );
+      })}
+    </select>
+  );
+};
+
+export default DropdownMenu;
